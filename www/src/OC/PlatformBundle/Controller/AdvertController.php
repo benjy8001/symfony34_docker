7fc050a1bd6f17cac1516cd595cf0a043e3c42c0
Benjamin Mabille <benjamin.mabille@ugloo.com>
2018-08-06 10:18:04 +0200

    Add Advert controller/route/view with simple Hello World

---
+++ www/src/OC/PlatformBundle/Controller/AdvertController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OC\PlatformBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdvertController extends Controller
+{
+	public function indexAction()
+	{
+		$content = $this
+				->get('templating')
+				->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'Benjy'));
+
+		return new Response($content);
+	}
+}

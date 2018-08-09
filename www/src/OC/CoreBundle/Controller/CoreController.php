28b406932ac6a2eb63e7f584945f0ed16b0560d3
Benjamin Mabille <benjamin.mabille@ugloo.com>
2018-08-09 18:20:37 +0200

    Add CoreBundle

---
+++ www/src/OC/CoreBundle/Controller/CoreController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace OC\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class CoreController extends Controller
+{
+    public function indexAction()
+    {
+		return $this->render('OCCoreBundle:Core:index.html.twig');
+    }
+
+    public function contactAction(Request $request)
+    {
+		$request->getSession()->getFlashBag()->add('notice', 'La page de contact n\'est pas encore disponible. Merci de revenir plus tard.');
+		return $this->redirectToRoute('oc_core_homepage');
+    }
+}

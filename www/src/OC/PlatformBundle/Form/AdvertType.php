854d196944e4f38e04cabce6fde88158cdf2bae2
Benjamin Mabille <benjamin.mabille@ugloo.com>
2018-09-03 10:15:39 +0200

    Add CkeditorType input form

--- www/src/OC/PlatformBundle/Form/AdvertType.php
+++ www/src/OC/PlatformBundle/Form/AdvertType.php
@@ -9,7 +9,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
@@ -27,7 +26,7 @@ class AdvertType extends AbstractType
 			->add('date',      DateTimeType::class)
 			->add('title',     TextType::class)
 			->add('author',    TextType::class)
-			->add('content',   TextareaType::class)
+			->add('content',   CkeditorType::class)
 			->add('image',     ImageType::class)
 			->add('categories', CollectionType::class, array(
 				'entry_type'   => CategoryType::class,
